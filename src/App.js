02a91067441cad024102303afa006f854c782617
Alex De Luca <alexnaj88@gmail.com>
2022-02-02 17:45:37 -0500

    changing app to understand base test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import "./App.css";
 import MainContent from "../src/components/main_content/MainContent.js";
 
 function App() {
-  return <MainContent />;
+  return (
+    <>
+      <h1 data-testid="sample-header">Sample E-Commerce</h1>
+      {/* <MainContent /> */}
+    </>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders basic h1 tag', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId("sample-header");
   expect(linkElement).toBeInTheDocument();
 });
