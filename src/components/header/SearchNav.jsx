683f78ca10afcd0bfa4696384ff35130e64a1233
Alex De Luca <alexnaj88@gmail.com>
2022-02-10 18:24:43 -0500

    base styling and flexing - logo needs to be routed

--- src/components/header/SearchNav.jsx
+++ src/components/header/SearchNav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./search_nav.sass";
 
@@ -10,27 +11,28 @@ const SearchNav = () => {
       * Logo+Name/Home link - Search Bar - Icon+Store/Location - Icon+Profile - Icon+Cart
       * ^ These probably SHOULD be broken up into their own components
       */}
-        <div data-testid="search-nav-logo-div">
-          <span id="search-nav-logo">&gt;./</span>
-          <span id="search-nav-logo-text">sample</span>
-        </div>
-        <div>
-          <input type="text" placeholder="Search products..." data-testid="search-nav-search"/>
-          <button /*type="submit"/</div>*/>Search</button>
-        </div>
+      <div id="search-nav-logo" data-testid="search-nav-logo">
+        <span id="search-nav-logo-icon">&gt;./</span>
+        <span id="search-nav-logo-text">sample</span>
+      </div>
+      <div id="search-nav-search">
+        <input id="searchbar" type="text" placeholder="Search products" data-testid="searchbar"/>
+        <button id="searchbar-button"/*type="submit"/</div>*/>Search</button>
+      </div>
+      <div id="search-nav-infos" data-testid="search-nav-infos">
         <div>
           {/* Pin icon prior */}
-          [City Example]
+          <span className="SearchNavInfo">? City Example</span>
         </div>
         <div>
           {/* Profile icon prior */}
-          Profile
+          <span className="SearchNavInfo">(^) Profile</span>
         </div>
         <div>
           {/* Shopping cart icon prior */}
-          <span id="search-nav-cart-num" data-testid="search-nav-cart-num">1</span>
-          <span>Cart</span>
+          <span className="SearchNavInfo">\--,<span id="search-nav-cart-num" data-testid="search-nav-cart-num">1</span> Cart</span>
         </div>
+      </div>
     </div>
   );
 };
