727d7f4e1e51ad3cc1e96ef4f7a942ac39126467
Alex De Luca <alexnaj88@gmail.com>
2022-02-10 23:42:15 -0500

    reformatting some components to routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import "./App.sass";
-
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import "./App.sass";
 import Header from "../src/components/header/Header.jsx";
-import PageContent from "../src/components/page_content/PageContent.jsx";
+import PageContent from "./routes/page_content/PageContent.jsx";
 
 function App() {
   return (
     <>
       <div data-testid="app-div">
-        <Header />
-        <PageContent />
+        <BrowserRouter>
+          <Header />
+          <PageContent />
+        </BrowserRouter>
       </div>
     </>
   );
